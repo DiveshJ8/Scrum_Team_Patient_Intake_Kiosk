0896db440f2956d560dd7d8c7f98131ea590612c
Abhay Patel <abhay@hisescientific.com>
2021-11-30 21:07:58 -0500

    Added schema

--- Kiosk/config/routes.rb
+++ Kiosk/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :sessions
+  resources :patients
+  resources :clients
+  resources :users
   resources :health_histories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- Kiosk/db/schema.rb
+++ Kiosk/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_203658) do
+ActiveRecord::Schema.define(version: 2021_11_22_170146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.integer "client_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "health_histories", force: :cascade do |t|
     t.integer "health_history_id"
@@ -22,6 +29,58 @@ ActiveRecord::Schema.define(version: 2021_11_18_203658) do
     t.string "status"
     t.string "value_type"
     t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.integer "patient_id"
+    t.integer "user_id"
+    t.string "license_no"
+    t.string "sex"
+    t.integer "height"
+    t.integer "weight"
+    t.string "marital_status"
+    t.string "race"
+    t.string "ethnicity"
+    t.string "income_group"
+    t.integer "emergency_contact_number"
+    t.string "emergency_contact_name"
+    t.string "emergency_contact_address"
+    t.string "emergency_contact_email"
+    t.string "rx_group"
+    t.string "rx_bin"
+    t.string "rx_pcn"
+    t.boolean "blue_button_approval"
+    t.string "visit_detail"
+    t.boolean "legal_consent"
+    t.string "insurance_no"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "session_id"
+    t.integer "user_id"
+    t.datetime "expiration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "client_id"
+    t.string "password"
+    t.datetime "last_login"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "dob"
+    t.string "email"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.integer "phone_no"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
