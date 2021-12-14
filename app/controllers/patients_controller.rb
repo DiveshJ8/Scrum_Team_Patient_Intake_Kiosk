8777447c6d3a37edd3be886343c2accfa29f45df
Diveshj8 <dj8@njit.edu>
2021-12-14 13:17:45 -0500

    healthinsurance API

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -73,7 +73,9 @@ class PatientsController < ApplicationController
 
   # GET /intake/:id/insuranceDetails get patient insurance details
   def insuranceDetails
-	patient = Patient.select(:id,:user_id,:insurance_no,:rx_group,:rx_bin,:rx_pcn, :provider, priority).find(params[:id])
+	#patient = Patient.select(:id,:user_id,:insurance_no,:rx_group,:rx_bin,:rx_pcn, :provider, priority).find(params[:id])
+	
+	patient = Patient.select(:healthinsurance_id, :insurer_name, :patient_id, :serial_id).find(params[:id])
 	render json: {"patient" => patient}, status: :ok
   end
 
@@ -135,6 +137,6 @@ def blueButtonUpdate
 
     # Only allow a list of trusted parameters through.
     def patient_params
-      params.require(:patient).permit(:id,:user_id, :license_no, :sex, :height, :weight,:marital_status, :race, :ethnicity,:income_group,:emergency_contact_number, :emergency_contact_name,:emergency_contact_address,:emergency_contact_email, :rx_group, :rx_bin, :rx_pcn, :blue_button_approval, :visit_detail, :legal_consent, :insurance_no, :provider, :priority)
+      params.require(:patient).permit(:id,:user_id, :license_no, :sex, :height, :weight,:marital_status, :race, :ethnicity,:income_group,:emergency_contact_number, :emergency_contact_name,:emergency_contact_address,:emergency_contact_email, :rx_group, :rx_bin, :rx_pcn, :blue_button_approval, :visit_detail, :legal_consent, :insurance_no, :provider, :priority, :healthinsurance_id, :insurer_name, :patient_id, :serial_id)
     end
 end
